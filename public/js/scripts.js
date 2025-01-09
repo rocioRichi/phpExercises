51af3676c2b2e8ed5a2d47a47dfd6a0bb48423bc
rocioRichi <paragit@gmail.com>
2025-01-09 10:54:56 +0100

    Undo lastcommit

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,32 +1,32 @@
 const exercises = ["exercise1", "exercise2", "exercise3"];
 
-// let currentExerciseIndex = 0;
+let currentExerciseIndex = 0;
 
-// function loadExercise(index) {
-//   const exercise = exercises[index];
-//   if (!exercise) {
-//     alert("Ejercicio no encontrado.");
-//     return;
-//   }
-//   const url = `https://phpexercises.onrender.com/exercise-index.php?`;
-//   fetch(url)
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! status: ${response.status}`);
-//       }
-//       return response.text();
-//     })
-//     .then((data) => {
-//       document.getElementById("exercise-container").innerHTML = data;
-//       currentExerciseIndex = index;
-//     })
-//     .catch((error) => {
-//       console.error("Error al cargar el ejercicio:", error);
-//       document.getElementById(
-//         "exercise-container"
-//       ).innerHTML = `<p>Error: ${error.message}</p>`;
-//     });
-// }
+function loadExercise(index) {
+  const exercise = exercises[index];
+  if (!exercise) {
+    alert("Ejercicio no encontrado.");
+    return;
+  }
+  const url = `https://phpexercises.onrender.com/exercise-index.php?`;
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then((data) => {
+      document.getElementById("exercise-container").innerHTML = data;
+      currentExerciseIndex = index;
+    })
+    .catch((error) => {
+      console.error("Error al cargar el ejercicio:", error);
+      document.getElementById(
+        "exercise-container"
+      ).innerHTML = `<p>Error: ${error.message}</p>`;
+    });
+}
 
 // Manejo de navegación
 document.getElementById("prev-button").addEventListener("click", () => {
@@ -69,4 +69,4 @@ function loadExerciseByUrl(url) {
 }
 
 // Cargar el primer ejercicio al inicio
-// loadExercise(currentExerciseIndex);
+loadExercise(currentExerciseIndex);
