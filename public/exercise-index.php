9bf53dbb16884411458281ded01e55f00098a026
Rocio Ricciardiello <mailparagit@gmail.com>
2025-02-11 13:16:40 +0100

    Adding again the new exercises to index

--- public/exercise-index.php
+++ public/exercise-index.php
@@ -5,7 +5,8 @@ $exercises = [
     ["id" => "exercise1", "title" => "Sumar dos números"],
     ["id" => "exercise2", "title" => "Mostrar una tabla"],
     ["id" => "exercise3", "title" => "Formulario de entrada"],
-
+    ["id" => "1", "title" => "prueba1/2"],
+    ["id" => "1Destino", "title" => "prueba2/2"],
 ];
 
 echo "<h1>Índice de Ejercicios</h1>";
